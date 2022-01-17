2e09142c82e0ef1cd14f8d8a4a4ae4c325297906
SureshYarram <sureshchowdary540@gmail.com>
2022-01-17 18:13:24 +0530

    deploy

---
+++ components/sidebar.js
@@ -0,0 +1,14 @@
+function sidebar() {
+
+
+    // return your html component here
+    //Make sure to give input search box id as ""
+
+    return `<div>
+    <h2>Search Here</h2>
+    <input type="text" id="searchbar" placeholder="Search News" />
+          <button id="searchbutton">Search</button>
+  
+  </div>`
+}
+export default sidebar
---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+async function apiCall(url) {
+    //add api call logic here
+  let res=await fetch(url);
+  let data =await res.json();
+ // console.log(data.articles);
+  return data.articles;
+
+}
+async function getData(term){
+    let res=await fetch(`https://gnews.io/api/v4/search?q=${term}&token=70a22b505169704e5cad137a2eb891cf`);
+    let data=res.json();
+    console.log(data);
+}
+
+function appendArticles(articles, main) {
+    main.innnerHTML="";
+    //add append logic here
+    articles.map(({title,content,description,image})=>{
+        let maindiv=document.createElement("div");
+     let div=document.createElement("div");
+     let div1=document.createElement("div");
+     let heading=document.createElement("h3");
+     heading.textContent=title;
+     let contentdetails=document.createElement("p");
+     contentdetails.textContent=content;
+     let img=document.createElement("img");
+     img.src=image;
+     div.append(heading,contentdetails);
+     div1.append(img);
+     maindiv.append(div,div1);
+     main.append(maindiv);
+     let article={
+        title,
+        content,
+        description,
+        image
+     }
+     maindiv.onclick=()=>{
+         saveArticle(article);
+     }
+      
+    })
+
+}
+function saveArticle(article){
+    localStorage.setItem("article",JSON.stringify(article));
+    window.location.href="news.html";
+}
+function newsAppend(article,newsmain,heading,image,content){
+     newsmain.innnerHTML="";
+    
+     heading.textContent=article.title;
+     image.src=article.image;
+     let p=document.createElement("p");
+    p.textContent=article.content+"\n"+article.description;
+    content.append(p)
+    newsmain.append(heading,image,content);
+}
+
+export { apiCall, appendArticles ,newsAppend ,getData}
